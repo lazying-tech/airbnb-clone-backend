39dca152eb63d24f5476f1a36178128cf211760e
lazying-tech <boylmhtsingle@gmail.com>
2023-04-20 21:23:32 +0700

    deploy 11

--- src/services/ListingService/ListingService.ts
+++ src/services/ListingService/ListingService.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 export const ListingService = {
-  create: async (createListing: CreatListingDto) => {
+  create: async (createListing: any) => {
     try {
       const {
         category,
