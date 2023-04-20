1e6f55332b3866a9c108380f88e34562e46af4ac
lazying-tech <boylmhtsingle@gmail.com>
2023-04-20 10:28:41 +0700

    deploy 1

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const PORT: any = process.env.PORT || 8000;
+app.use("/api/v1", MainRoute);
+
 app.use("/", (req: Request, res: Response) => {
   return res.json("AKFJL");
 });
-app.use("/api/v1", MainRoute);
-
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
