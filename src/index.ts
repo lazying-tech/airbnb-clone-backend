09b2363b897bc11fae18e82d1222dbbb922129ee
lazying-tech <boylmhtsingle@gmail.com>
2023-04-20 10:24:46 +0700

    Second  commit

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const PORT: any = process.env.PORT || 8000;
-
+app.use("/", () => {
+  console.log("Hello");
+});
 app.use("/api/v1", MainRoute);
 
 app.listen(PORT, () => {
