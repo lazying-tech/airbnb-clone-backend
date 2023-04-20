63e3692343ff3349ccde2eb2931bf1951f0911d6
lazying-tech <boylmhtsingle@gmail.com>
2023-04-20 11:05:12 +0700

    deploy 4

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import MainRoute from "./routes/index";
 import cors from "cors";
 require("dotenv").config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
